550bd6ad6e84a03a3b694bc191aceaa977c94465
threatt-zm <10990825@live.mercer.edu>
2025-06-26 05:17:41 -0400

    added collision detection

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 from constants import *
 from player import Player
 from asteroid import Asteroid
@@ -31,7 +32,13 @@ def main():
 
         for dw in drawable:
             dw.draw(screen)
+
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides(player):
+                print("Game over!")
+                sys.exit()
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
